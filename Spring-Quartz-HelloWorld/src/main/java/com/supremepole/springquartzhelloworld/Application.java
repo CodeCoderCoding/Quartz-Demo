52845afc2033bea6fccfb3e0b97cfb395b001d39
Neil <1845371176@qq.com>
2023-09-24 13:12:27 +0800

    feat(Spring-Quartz-HelloWorld): add spring quartz hello world demo

---
+++ Spring-Quartz-HelloWorld/src/main/java/com/supremepole/springquartzhelloworld/Application.java
@@ -0,0 +1,10 @@
+package com.supremepole.springquartzhelloworld;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        context.start();
+    }
+}
